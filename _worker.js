54bbfcf148ce4cc5ca04f9efc6a15ab5d27a175b
merryf6 <124674099+merryf6@users.noreply.github.com>
2024-05-13 10:34:40 +0330

    Add files via upload

---
+++ _worker.js
@@ -0,0 +1,18 @@
+addEventListener(
+  "fetch", event => {
+      
+      const ip = event.request.headers.get('cf-connecting-ip') || event.request.headers.get('x-forwarded-for') || (event.request.socket && event.request.socket.remoteAddress);
+      let url = new URL(event.request.url);
+      console.log(event.request.body)
+      const worker_domain=url.hostname;
+      url.hostname = "dl.diginetplan.cloud";                        
+      url.protocol = event.request.headers.get('x-forwarded-proto') || "https";
+      let request = new Request(url, event.request);
+      if (ip)
+       request.headers.set('cf-connecting-ip', ip);
+       request.headers.set('Host', worker_domain);
+      event.respondWith(
+          fetch(request)
+      )
+  }
+)
